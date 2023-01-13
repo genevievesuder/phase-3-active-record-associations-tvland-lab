96fa8606ca7a1211813d389f6d4a78a079072551
Genevieve Suder <genevievesuder@gmail.com>
2023-01-13 15:26:47 -0800

    commit tree fe0fac7c1ad0586a61065c6c9d8d7f00a6bf69e2

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
+require 'pry'
+require_relative "../../config/environment"
+
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.actors.map(&:full_name)
+  end
+
 end
+binding.pry
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,11 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    create_table :characters do |t|
+      t.string :name
+      t.belongs_to :actor
+      t.belongs_to :show
+
+      t.timestamps
+    end
   end
 end
